22a88c401fab653e6651b93f298423c5115e0faf
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-02-04 11:37:13 +0100

    Clean up of the main code, utilizing new classes and functions. Removed old uwb2position fuctions that were no longer required

--- Estimator_MovingHorizon.m
+++ Estimator_MovingHorizon.m
@@ -1,160 +1,152 @@
 %% Moving Horizon Estimator from Crazyflie logs
 % input: data structure, fields: x,y,z,vx,vy,vz,pitch,roll,yaw
 close all;
-clear all;
+%clear all;
 addpath('Functions');
-load('Data/square.mat')
+addpath('Classes');
+%load('Data/square.mat')
+load('simulationData_test.mat');
 %% Parameters
-mode = 'multi';
-pos_update_dt = 0.1;
+pos_update_dt = 0.01;
 mhe_timeHorizon = 0.5; % s
 ransac_iter = 10;
 ransac_sampleSize = 2;
 ransac_outlierThreshold = 0.05;
-PV = 1;
+PV = 10;
 ransac_prior = [0 0; 
                 0 PV];
             
 MAX_WINDOW_SIZE = 100;
 N = length(data.time);
-const_g = 9.81;
-const_k_aero = 0.35;
+k_aero = 0.35;
+g = 9.81;
+
+% initialize objects
+state_prediction = QuadrotorState();
+state_corrector = QuadrotorState();
+state_estimate = QuadrotorState();
 
 % initial values
-prediction.p = [data.x(1),data.y(1),data.z(1)];
-prediction.v = [0,0,0];
-state.p = prediction.p;
-state.v = prediction.v;
-log.x(1) = state.p(1);
-log.y(1) = state.p(2);
-log.z(1) = state.p(3);
-log.vx(1) = state.v(1);
-log.vy(1) = state.v(2);
-log.vz(1) = state.v(3);
-last_uwb_timestamp = -1;
-dpos_w = zeros(MAX_WINDOW_SIZE,3);
-time_w = zeros(MAX_WINDOW_SIZE,1);
-windowSize = 0;
-corrector.p = [0,0,0];
-corrector.v = [0,0,0];
+pos0 = [data.x(1),data.y(1),data.z(1)];
+state_prediction.p = pos0;
+state_estimate.p = pos0;
+predictor.p0 = pos0;
 
-% convert time to seconds
-data.time = data.time/1000;
-for i = 1:length(data.uwb)
-    data.uwb(i).timestamp = data.uwb(i).timestamp/1000;
-end
+log.x(1) = state_estimate.p(1);
+log.y(1) = state_estimate.p(2);
+log.z(1) = state_estimate.p(3);
+log.vx(1) = state_estimate.v(1);
+log.vy(1) = state_estimate.v(2);
+log.vz(1) = state_estimate.v(3);
+
+% MHE windows
+wPrediction = zeros(MAX_WINDOW_SIZE,3);
+wMeasurement = zeros(MAX_WINDOW_SIZE,3);
+wTime = zeros(MAX_WINDOW_SIZE,1);
+windowSize = 0;
 
 uwb_count = 1;
+mode = data.uwb(1).rangingMode;
 
-for i=291:N
-    if(mod(data.time(i),pos_update_dt) == 0)
-        % no estimation of attitude yet
-        state.pitch = data.pitch(i);
-        state.roll = data.roll(i);
-        state.yaw = data.yaw(i);
-    
-        % z initial estimate not by mhe
-        state.p(3) = data.z(i);
-        state.v(3) = (data.z(i)-data.z(i-1))/(data.time(i)-data.time(i-1));
+%% Simulation
+for i=10:N
+    time_now = data.time(i);
+    if(mod(time_now,pos_update_dt) == 0)
+        % Prediction 
+        pitch_global = data.pitch(i) * cos(data.yaw(i)) - data.roll(i)*sin(data.yaw(i));
+        roll_global = data.pitch(i) * sin(data.yaw(i)) + data.roll(i)*cos(data.yaw(i));
         
-        % predict current state
-        pitch_global = state.pitch * cos(state.yaw) - state.roll*sin(state.yaw);
-        roll_global = state.pitch * sin(state.yaw) + state.roll*cos(state.yaw);
+        state_prediction.p(1) = state_prediction.p(1) + state_prediction.v(1) * pos_update_dt;
+        state_prediction.p(2) = state_prediction.p(2) + state_prediction.v(2) * pos_update_dt;
         
-        prediction.p(1) = prediction.p(1) + prediction.v(1) * pos_update_dt;
-        prediction.p(2) = prediction.p(2) + prediction.v(2) * pos_update_dt;
-        prediction.p(3) = state.p(3);
-        
-        %prediction.v(1) = data.vx(i);
-        %prediction.v(2) = data.vy(i);
-        %prediction.v(3) = data.vz(i);
-        
-        prediction.v(1) = prediction.v(1) + (-const_g*tan(pitch_global) - const_k_aero*state.v(1))*pos_update_dt;
-        prediction.v(2) = prediction.v(2) + (const_g*tan(roll_global) - const_k_aero*state.v(2))*pos_update_dt;
-        prediction.v(3) = state.v(3);
+        state_prediction.v(1) = state_prediction.v(1)...
+                                + (-g*tan(pitch_global) - k_aero*state_prediction.v(1)) * pos_update_dt;
+        state_prediction.v(2) = state_prediction.v(2)...
+                                + (g*tan(roll_global) - k_aero*state_prediction.v(2)) * pos_update_dt;
+        log.prediction(i,:) = state_prediction.p; 
         
         % Measurement
         measurement_update = 0;
-        if(strcmp(mode,'single'))
-            while (uwb_count <= length(data.uwb) && data.uwb(uwb_count).timestamp <= data.time(i))
-                if (data.uwb(uwb_count).timestamp == data.time(i))
-                    measurement_update = 1;
-                    if (strcmp(data.uwb(uwb_count).mode,'tdoa'))
-                        measurement = tdoa2position_s(data.uwb(uwb_count),prediction.p);
-                    elseif(strcmp(data.uwb(uwb_count).mode,'twr'))
-                        measurement = twr2position_s(data.uwb(uwb_count),prediction.p);
-                    else
-                        break;
-                    end
-                    log.measurement.x(uwb_count) = measurement(1);
-                    log.measurement.y(uwb_count) = measurement(2);
-                    log.measurement.z(uwb_count) = measurement(3);
-                    % Window Updates
-                    dpos_w = circshift(dpos_w,1,1);
-                    time_w = circshift(time_w,1,1);
-                    dpos_w(1,:) = measurement - prediction.p;
-                    time_w(1) = data.time(i);
-
-                    if (windowSize < MAX_WINDOW_SIZE)
-                        windowSize = windowSize + 1;
-                    end
-                    while ((time_w(1)-time_w(windowSize)) > mhe_timeHorizon)
-                        windowSize = windowSize -1;
-                    end     
-                end
+        %%%%%%%%%%%
+        % Multilateration & Variations
+        if mode == rangingModes.twr
+            while (uwb_count <= length(data.uwb) && data.uwb(uwb_count).timestamp <= time_now)
+                uwb_mem(data.uwb(uwb_count).anchorID + 1) = data.uwb(uwb_count);
                 uwb_count = uwb_count + 1;
+                measurement_update = 1;
             end
-        elseif(strcmp(mode,'multi'))
-            while (uwb_count <= length(data.uwb) && data.uwb(uwb_count).timestamp <= data.time(i))
-                newUWB = data.uwb(uwb_count);
-                twr_mem(newUWB.id + 1) = newUWB;
-                uwb_count = uwb_count + 1;     
-            end
-    
-            measurement = twr2position_m(twr_mem(1),twr_mem(2),twr_mem(3),twr_mem(4));
-            log.measurement.x(uwb_count) = measurement(1);
-            log.measurement.y(uwb_count) = measurement(2);
-            log.measurement.z(uwb_count) = measurement(3);
-            % Window Updates
-            dpos_w = circshift(dpos_w,1,1);
-            time_w = circshift(time_w,1,1);
-            dpos_w(1,:) = measurement - prediction.p;
-            time_w(1) = data.time(i);
+            
+        elseif mode == rangingModes.tdoa
 
+            while (uwb_count <= length(data.uwb) && data.uwb(uwb_count).timestamp <= time_now)
+                idxA = data.uwb(uwb_count).anchorID_A + 1;
+                idxB = data.uwb(uwb_count).anchorID_B + 1;
+                if idxA>idxB
+                    data.uwb(uwb_count).switchAnchors();
+                    idxA = data.uwb(uwb_count).anchorID_A + 1;
+                    idxB = data.uwb(uwb_count).anchorID_B + 1;
+                end
+                uwb_mat(idxA,idxB) = data.uwb(uwb_count);
+                uwb_logical(idxA,idxB) = true;
+                uwb_count = uwb_count + 1;
+                measurement_update = 1;
+            end
+            uwb_mem = uwb_mat(uwb_logical);
+        end
+        
+        measurement = uwbMultilat(uwb_mem, 'prior', state_estimate.p,'nbest',10);%, 'forceZ', data.z(i));
+        
+        %%%%%%%%%%%
+        % Single Measurement Projection & Variations
+%         prior = state_estimate.p;
+%         while (uwb_count <= length(data.uwb) && data.uwb(uwb_count).timestamp <= time_now)
+%             prior = uwbProject(data.uwb(uwb_count), prior,'forceZ',data.z(i));
+%             uwb_count = uwb_count + 1;
+%         end
+%         measurement = prior;
+        %%%%%%%%%%%
+        
+        log.measurement.x(i) = measurement(1);
+        log.measurement.y(i) = measurement(2);
+        log.measurement.z(i) = measurement(3);
+        
+        if measurement_update
+            % Update moving windows
+            wPrediction = [state_prediction.p; wPrediction(1:end-1,:)];
+            wMeasurement = [measurement; wMeasurement(1:end-1,:)];
+            wTime = [time_now; wTime(1:end-1)];
+            
             if (windowSize < MAX_WINDOW_SIZE)
                 windowSize = windowSize + 1;
             end
-            while ((time_w(1)-time_w(windowSize)) > mhe_timeHorizon)
+            while ((wTime(1)-wTime(windowSize)) > mhe_timeHorizon)
                 windowSize = windowSize -1;
-            end     
-        else
-            break;
-        end
+            end
+            
+            % calculate delta windows
+            wDt = wTime - wTime(windowSize);
+            wDp = wMeasurement - wPrediction;
 
-        
-        
-        if measurement_update
-            dt_w = time_w - time_w(windowSize);
-
-            if(windowSize >= ransac_sampleSize && sum(dt_w>0)>ransac_sampleSize-1)
-                best_error = windowSize * ransac_outlierThreshold;
+            % RANSAC MHE
+            if(windowSize >= ransac_sampleSize && sum(wDt>0)>ransac_sampleSize-1)
+                best_error = windowSize * ransac_outlierThreshold + 1;
 
                 for j=1:ransac_iter
                     idx = randsample(windowSize,ransac_sampleSize);
 
                     if (ransac_sampleSize == 2)
-                        denom = (dt_w(idx(2)) - dt_w(idx(1)))^2 + 2*PV;
+                        denom = (wDt(idx(2)) - wDt(idx(1)))^2 + 2*PV;
 
-                        s_dp = (( dpos_w(idx(1),:) * dt_w(idx(2)) - dpos_w(idx(2),:) * dt_w(idx(1)) ) ...
-                                * (dt_w(idx(2)) - dt_w(idx(1))) + (dpos_w(idx(1),:) + dpos_w(idx(2),:)) * PV)/denom;
-                        s_dv = ( dpos_w(idx(2),:) - dpos_w(idx(1),:) ) * (dt_w(idx(2)) - dt_w(idx(1))) / denom;
+                        s_dp = (( wDp(idx(1),:) * wDt(idx(2)) - wDp(idx(2),:) * wDt(idx(1)) ) ...
+                                * (wDt(idx(2)) - wDt(idx(1))) + (wDp(idx(1),:) + wDp(idx(2),:)) * PV)/denom;
+                        s_dv = ( wDp(idx(2),:) - wDp(idx(1),:) ) * (wDt(idx(2)) - wDt(idx(1))) / denom;
                     else
-                        [s_dp(1),s_dv(1)] = linearLeastSquares(dt_w(idx),dpos_w(idx,1),ransac_prior);
-                        [s_dp(2),s_dv(2)] = linearLeastSquares(dt_w(idx),dpos_w(idx,2),ransac_prior);
-                        [s_dp(3),s_dv(3)] = linearLeastSquares(dt_w(idx),dpos_w(idx,3),ransac_prior);
+                        [s_dp(1),s_dv(1)] = linearLeastSquares(wDt(idx),wDp(idx,1),ransac_prior);
+                        [s_dp(2),s_dv(2)] = linearLeastSquares(wDt(idx),wDp(idx,2),ransac_prior);
+                        [s_dp(3),s_dv(3)] = linearLeastSquares(wDt(idx),wDp(idx,3),ransac_prior);
                     end
 
-                    step_error = s_dv.*dt_w(1:windowSize) + s_dp - dpos_w(1:windowSize,:);
+                    step_error = s_dv.*wDt(1:windowSize) + s_dp - wDp(1:windowSize,:);
                     step_error = sqrt(sum(step_error.^2,2)); % mean squared error for each timestep
                     isOutlier = (step_error>ransac_outlierThreshold); % identify outliers
                     step_error(isOutlier) = ransac_outlierThreshold; % prune outlier error
@@ -171,36 +163,36 @@ for i=291:N
 
                 % Recalculate best model from inliers
                 if (sum(~best_isOutlier)>=2)
-                    dpos_in = dpos_w(~best_isOutlier,:);
-                    dt_in = dt_w(~best_isOutlier);
+                    dpos_in = wDp(~best_isOutlier,:);
+                    dt_in = wDt(~best_isOutlier);
 
-                    [corrector.p(1), corrector.v(1)] = linearLeastSquares(dt_in,dpos_in(:,1),ransac_prior);
-                    [corrector.p(2), corrector.v(2)] = linearLeastSquares(dt_in,dpos_in(:,2),ransac_prior);
-                    [corrector.p(3), corrector.v(3)] = linearLeastSquares(dt_in,dpos_in(:,3),ransac_prior);
+                    [state_corrector.p(1), state_corrector.v(1)] = linearLeastSquares(dt_in,dpos_in(:,1),ransac_prior);
+                    [state_corrector.p(2), state_corrector.v(2)] = linearLeastSquares(dt_in,dpos_in(:,2),ransac_prior);
+                    [state_corrector.p(3), state_corrector.v(3)] = linearLeastSquares(dt_in,dpos_in(:,3),ransac_prior);
                 else
-                    corrector.p = best_dp;
-                    corrector.v = best_dv;
+                    state_corrector.p = best_dp;
+                    state_corrector.v = best_dv;
                 end
 
             end
 
         end
         if windowSize > 0
-            delta_t = (data.time(i) - time_w(windowSize))/1000;
+            delta_t = (data.time(i) - wTime(windowSize))/1000;
         else
             delta_t = data.time(i);
         end
-        state.p = prediction.p + corrector.p + corrector.v*delta_t;
-        state.v = prediction.v + corrector.v;
+        state_estimate.p = state_prediction.p + state_corrector.p + state_corrector.v*delta_t;
+        state_estimate.v = state_prediction.v + state_corrector.v;
     end
     
     log.time(i) = data.time(i);
-    log.x(i) = state.p(1);
-    log.y(i) = state.p(2);
-    log.z(i) = state.p(3);
-    log.vx(i) = state.v(1);
-    log.vy(i) = state.v(2);
-    log.vz(i) = state.v(3);
+    log.x(i) = state_estimate.p(1);
+    log.y(i) = state_estimate.p(2);
+    log.z(i) = state_estimate.p(3);
+    log.vx(i) = state_estimate.v(1);
+    log.vy(i) = state_estimate.v(2);
+    log.vz(i) = state_estimate.v(3);
 
 end
 
@@ -208,10 +200,10 @@ end
 % trajectory
 figure(1)
 subplot(2,2,1)
-axis([0,5,0,5,0,5])
-xticks([0,1,2,3,4,5])
+axis([-5,5,-5,5,0,5])
+xticks(-5:5)
 xlabel('x')
-yticks([0,1,2,3,4,5])
+yticks(-5:5)
 ylabel('y')
 zticks([0,1,2,3,4,5])
 zlabel('z')
@@ -223,10 +215,10 @@ plot3(data.x,data.y,data.z,'b')
 plot3(log.x,log.y,log.z,'r')
 % x-y
 subplot(2,2,2)
-axis([0,5,0,5,])
-xticks([0,1,2,3,4,5])
+axis([-5,5,-5,5,])
+xticks(-5:5)
 xlabel('x')
-yticks([0,1,2,3,4,5])
+yticks(-5:5)
 ylabel('y')
 daspect([1 1 1])
 hold('on')
@@ -236,8 +228,8 @@ plot(data.x,data.y,'b')
 plot(log.x,log.y,'r')
 % x-z
 subplot(2,2,3)
-axis([0,5,0,5])
-xticks([0,1,2,3,4,5])
+axis([-5,5,0,5])
+xticks(-5:5)
 xlabel('x')
 yticks([0,1,2,3,4,5])
 ylabel('z')
@@ -249,8 +241,8 @@ plot(data.x,data.z,'b')
 plot(log.x,log.z,'r')
 %  y-z
 subplot(2,2,4)
-axis([0,5,0,5])
-xticks([0,1,2,3,4,5])
+axis([-5,5,0,5])
+xticks(-5:5)
 xlabel('y')
 yticks([0,1,2,3,4,5])
 ylabel('z')
