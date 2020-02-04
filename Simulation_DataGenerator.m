d9e509d986192e296ed20e54dd4cecfa301f6022
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-02-04 11:20:53 +0100

    Update to data generator to work with new classes, removed no longer needed functions to get UWB measurements

--- Simulation_DataGenerator.m
+++ Simulation_DataGenerator.m
@@ -1,13 +1,15 @@
 close all;
 clear all;
-addpath('Functions');
+addpath('Classes and Objects')
+addpath('Functions')
+addpath('Data Types')
 %% Inputs
 rng(0);
 N = 10000;
-dt = 10;   % in ms like real drone data
-uwb_range = 10;
-uwb_update_probability = 0.05; % determines avg frequency of uwb measurements
-uwb_mode = 'tdoa';      % 'tdoa' or 'twr'
+dt = 0.01;   % in s
+uwb_range = 20;
+uwb_update_probability = 0.9; % determines avg frequency of uwb measurements
+uwb_mode = rangingModes.tdoa;      % 'tdoa' or 'twr'
 
 % Noise
 noise.attitude = 0.05;
@@ -16,19 +18,23 @@ noise.uwb = 0.05;
 % Constants
 g = 9.81;
 k_aero = 0.35;
-% Anchors (one row per anchor, [x,y,z]
-anchors = [ 0, 0, 0
-            0, 0, 5;
-            0, 5, 0;
-            0, 5, 5;
-            5, 5, 0;
-            5, 5, 5;
-            5, 0, 0;
-            5, 0, 5];
+%% Anchors
+% remove anchor by comenting the line
+i = 1;
+% Cyberzoo-like setup
+anchors(i) = Anchor(0,[-5,-5, 1.7]); i=i+1;
+anchors(i) = Anchor(1,[-5, 5, 1.7]); i=i+1;
+anchors(i) = Anchor(2,[ 5, 5, 0.9]); i=i+1;
+anchors(i) = Anchor(3,[ 5,-5, 1.7]); i=i+1;
+% Cube setup
+anchors(i) = Anchor(4,[-5,-5, 0]); i=i+1;
+anchors(i) = Anchor(5,[-5, 5, 0]); i=i+1;
+anchors(i) = Anchor(6,[ 5, 5, 5]); i=i+1;
+anchors(i) = Anchor(7,[ 5,-5, 0]); i=i+1;
 
 %% Drone trajectory (ground truth)
 data.time = (linspace(0,N-1,N)*dt)';
-time = data.time/1000; % in seconds for trajectory calculations
+time = data.time;
 %3rd order polynomial
 % p0 = [1,2,0];
 % p1 = [4,4,3];
@@ -37,13 +43,15 @@ time = data.time/1000; % in seconds for trajectory calculations
 % traj = trajSpline(p0,p1,v0,v1,time);
 
 % setpoints
-points = [3, 3, 0;
-          3, 3, 1;
-          1, 1, 1;
-          1, 4, 2;
-          4, 4, 2;
-          4, 1, 1;
-          1, 1, 1];
+points = [ 0, 0, 0;
+           0, 0, 2;
+          -3,-3, 2;
+          -3, 3, 2;
+           3, 3, 2;
+           3,-3, 2;
+          -3,-3, 2;
+           0, 0, 2;
+           0, 0, 0];
 traj = trajPoints(points, time);
 
 data.x = traj(:,1);
@@ -54,9 +62,9 @@ data.vx = zeros(N,1);
 data.vy = zeros(N,1);
 data.vz = zeros(N,1);
 for i=2:N
-    data.vx(i) = (data.x(i)-data.x(i-1))/(dt/1000);
-    data.vy(i) = (data.y(i)-data.y(i-1))/(dt/1000);
-    data.vz(i) = (data.z(i)-data.z(i-1))/(dt/1000);
+    data.vx(i) = (data.x(i)-data.x(i-1))/dt;
+    data.vy(i) = (data.y(i)-data.y(i-1))/dt;
+    data.vz(i) = (data.z(i)-data.z(i-1))/dt;
 end
 
 %% Drone attitude (reverse model)
@@ -75,29 +83,27 @@ end
 
 %% UWB
 position = [data.x, data.y, data.z];
-anch_inRange = anchorsInRange(anchors,position(1,:),uwb_range);
-if (strcmp(uwb_mode,'twr'))
-    data.uwb = getTWR(anch_inRange,position(1,:),time(1));
-    data.uwb.distance = data.uwb.distance * (1 + noise.uwb * randn());
-elseif (strcmp(uwb_mode,'tdoa'))
-    data.uwb = getTDOA(anch_inRange,position(1,:),time(1));
-    data.uwb.dist_diff = data.uwb.dist_diff * (1 + noise.uwb * randn());
-end
-        
-for i=2:N
-    if (rand()<=uwb_update_probability)
-        anch_inRange = anchorsInRange(anchors,position(i,:),uwb_range);
-        if (strcmp(uwb_mode,'twr'))
-            data.uwb(i) = getTWR(anch_inRange,position(i,:),time(i));
-            data.uwb(i).distance = data.uwb(i).distance * (1 + noise.uwb * randn());
-        elseif (strcmp(uwb_mode,'tdoa'))
-            data.uwb(i) = getTDOA(anch_inRange,position(i,:),time(i));
-            data.uwb(i).dist_diff = data.uwb(i).dist_diff * (1 + noise.uwb * randn());
+uwb_count = 1;
+for i=1:N
+    for j=1:length(anchors)
+        if (rand()<=uwb_update_probability)
+            if uwb_mode == rangingModes.twr
+                dist = norm(position(i,:)-anchors(j).position) + noise.uwb * randn();
+                data.uwb(uwb_count) = TWRPacket(anchors(j),dist,data.time(i));
+                uwb_count = uwb_count + 1;
+            elseif uwb_mode == rangingModes.tdoa
+                anch2idx = randi(length(anchors));
+                while anch2idx == j
+                    anch2idx = randi(length(anchors));
+                end
+                dA = norm(position(i,:)-anchors(j).position);
+                dB = norm(position(i,:)-anchors(anch2idx).position);
+                dist = dA - dB + noise.uwb * randn();
+                data.uwb(uwb_count) = TDOAPacket(anchors(j),anchors(anch2idx),dist,data.time(i));
+                uwb_count = uwb_count + 1;
+            end
         end
-    else
-            data.uwb(i) = data.uwb(i-1);
     end
-    
 end
 
 %% Save data
