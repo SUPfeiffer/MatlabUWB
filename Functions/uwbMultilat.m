80be05f3933c5ae26338eccf235c9c621ce7c4d6
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-02-04 11:25:09 +0100

    Added unified functions for multilateration and projection from uwb measurements to take advantage of new classes and improve code readability

---
+++ Functions/uwbMultilat.m
@@ -0,0 +1,116 @@
+function position = uwbMultilat(uwb, varargin)
+%uwbMultilat: calculates position from uwb mesurements
+%   uwb: array of uwb measurements, both TWRPacket and TDOAPacket types are
+%           accepted
+%   position: the optimal position in the least square sense
+%
+%   name-value parameters:
+%   prior = [0,0,0]:    prior estimate of the position, not required if 
+%                       uwb measurements are of type TWRPacket
+%   timeout = Inf:      specifies the time of the oldest measurements that
+%                       should still be used
+%   nbest = Inf:        only consider the nbest best measurements
+
+
+    % read name-value parameters
+    p = inputParser();
+    addParameter(p,'prior',[0,0,0]);
+    addParameter(p,'timeout', 0);
+    addParameter(p,'nbest', inf);
+    addParameter(p,'forceZ', nan);
+    parse(p,varargin{:});
+    prior = p.Results.prior;
+    timeout = p.Results.timeout;
+    nbest = p.Results.nbest;
+    forceZ = p.Results.forceZ;
+
+    %% Pre-processing of measurements
+    % remove outdated measurements
+    if timeout
+        for i=length(uwb):1
+            if uwb(i).timestamp < timeout
+                uwb(i) = [];
+            end
+        end
+    end
+    % only keep the nbest best measurements
+    if ~isinf(nbest)
+        for i=1:length(uwb)
+            % Measure of the quality of a measurement could be changed, in
+            % reality, one could for example keep track of differences between
+            % measurement and prediction
+            quality(i) = uwb(i).timestamp;
+        end
+        for i=1:nbest
+            idx(i) = find(quality == max(quality),1);
+            quality(idx(i)) = -1;
+        end
+        uwb = uwb(idx);
+    end
+
+    %% Calculate position from pre-processed measurements
+    N = length(uwb);
+    mode = uwb(1).rangingMode;
+    if mode==rangingModes.twr
+        % TWR measurements can be arranged in a set of linear equations
+        % -> Analytical solution
+        for i=1:N
+        x(i) = uwb(i).anchorPosition(1);
+        y(i) = uwb(i).anchorPosition(2);
+        z(i) = uwb(i).anchorPosition(3);
+        d(i) = uwb(i).distance;
+        end
+        
+        if isnan(forceZ)
+            A = [ones(N,1), -2*x', -2*y', -2*z'];
+            b = d'.^2 -x'.^2 - y'.^2 - z'.^2;
+            res = A\b;
+            position = [res(2),res(3),res(4)];
+        else
+            % Force Z
+            A = [ones(N,1), -2*x', -2*y'];
+            b = d'.^2 - x'.^2 - y'.^2 - (z'-forceZ).^2;
+            res = A\b;
+            position = [res(2),res(3),forceZ];
+            
+            % "Suggest" Z (add as additional equation)
+%             A = [ones(N,1), -2*x', -2*y', -2*z'];
+%             A = [A; 0,0,0,1];
+%             b = d'.^2 -x'.^2 - y'.^2 - z'.^2;
+%             b = [b; forceZ];
+%             res = A\b;
+%             position = [res(2),res(3),res(4)];
+            
+        end
+        
+    elseif mode==rangingModes.tdoa
+        % No analytical solution for tdoa 
+        % -> numerical solution with Gauss-Newton
+        position = prior;
+        while true
+            for i=1:N
+                if ~isnan(forceZ)
+                    position(3) = forceZ;
+                end
+                
+                dA = position - uwb(i).anchorPosition_A;
+                dB = position - uwb(i).anchorPosition_B;
+                
+                S(i) = norm(dA) - norm(dB) - uwb(i).distance;
+                J(i,:) = dA/norm(dA) - dB/norm(dB);
+            end
+            
+            if rank(J)<3
+                break;
+            end
+            delta = J\S';
+            position = position - delta';
+            
+            if norm(delta) < 0.001
+                break;
+            end    
+        end
+        
+    end
+end
+
---
+++ Functions/uwbProject.m
@@ -0,0 +1,151 @@
+function position = uwbProject(uwb,prior,varargin)
+%uwbProject: calculates the projection of the prior position estimate onto
+%   the uwb measurement.
+%   
+%   uwb: uwb measurement as TWRPacket or TDOAPacket
+%   prior: prior estimate of the position
+
+% read name-value parameters
+    p = inputParser();
+    addParameter(p,'forceZ', nan);
+    parse(p,varargin{:});
+    forceZ = p.Results.forceZ;
+    
+    % calculation
+    mode = uwb.rangingMode;
+    if mode == rangingModes.twr
+        if isnan(forceZ)
+            dir = (prior-uwb.anchorPosition)/norm(prior-uwb.anchorPosition);
+            position = uwb.anchorPosition + uwb.distance * dir;
+        else
+            prior2D = prior(1:2);
+            anchor2D = uwb.anchorPosition(1:2);
+            distance2D = sqrt(uwb.distance^2 -(forceZ-uwb.anchorPosition(3))^2);
+            
+            dir = (prior2D-anchor2D)/norm(prior2D-anchor2D);
+            position2D = anchor2D + distance2D*dir;
+            position = [position2D, forceZ];
+        end
+    
+    elseif mode == rangingModes.tdoa
+        % turn anchor positions into column vectors
+        a1 = uwb.anchorPosition_A'; 
+        a2 = uwb.anchorPosition_B';
+        % variables for timeout if no convergence
+        timeout_max_i = 20; 
+        exit_timeout = 0;     
+        
+        if isnan(forceZ)
+            % Initialize optimization problem
+            X = [prior'; 0];    % Candidate Solution (position on tdoa measurement)
+            F = zeros(4,1);     % Objective function
+            while (true)
+
+                d1 = norm(X(1:3)-a1);
+                d2 = norm(X(1:3)-a2);
+
+                % normal vector to the tdoa measurement (hyperboloid) at candidate solution
+                gradS = (X(1:3)-a1)/d1 - (X(1:3)-a2)/d2;
+
+                % Objective function
+                F(1:3) = X(1:3) + X(4)*gradS - prior';   
+                F(4) = d1 - d2 - uwb.distance;
+
+                % Jacobian of the objective function
+                J(1:3,1:3) = eye(3) + X(4)*( (eye(3)/d1 - ((X(1:3)-a1)*(X(1:3)-a1)')/(d1^3)) ...
+                                            -(eye(3)/d2 - ((X(1:3)-a2)*(X(1:3)-a2)')/(d2^3)) );
+                J(1:3,4) = gradS;
+                J(4,1:4) = [gradS; 0]';
+
+                % update for the candidate solution
+                delta = J\F;
+
+                % backtracking line search to ensure convergence if prior is far
+                % from root
+                i = 0;
+                while(true)
+                    X_new = X - (0.5^i) * delta;
+
+                    d1 = norm(X_new(1:3)-a1);
+                    d2 = norm(X_new(1:3)-a2);
+                    gradS = (X_new(1:3)-a1)/d1 - (X_new(1:3)-a2)/d2;
+                    F_new(1:3) = X_new(1:3) + X_new(4)*gradS - prior';
+                    F_new(4) = d1 - d2 - uwb.distance;
+                    if (norm(F_new)<=norm(F))
+                        break;
+                    elseif (i>timeout_max_i)
+                        % timeout if no convergence for too long
+                        exit_timeout = 1;
+                        break;
+                    else
+                        i = i+1;
+                    end
+                end
+                X = X_new;
+                if norm(delta)<0.001
+                    break;
+                elseif (exit_timeout)
+                    % forced exit by timeout
+                    break;
+                end
+            end
+            position = X(1:3)';
+        else
+            % z forced, doesn't work properly yet!
+            prior(3) = forceZ;
+            X = [prior(1:2)'; 0];    % Candidate Solution (position on tdoa measurement)
+            F = zeros(3,1);     % Objective function
+            while (true)
+                d1 = norm([X(1:2);forceZ]-a1);
+                d2 = norm([X(1:2);forceZ]-a2);
+
+                % normal vector to the tdoa measurement (hyperboloid) at candidate solution
+                gradS = (X(1:2)-a1(1:2))/d1 - (X(1:2)-a2(1:2))/d2;
+
+                % Objective function
+                F(1:2) = X(1:2) + X(3)*gradS - prior(1:2)';   
+                F(3) = d1 - d2 - uwb.distance;
+
+                % Jacobian of the objective function
+                J(1:2,1:2) = eye(2) + X(3)*( (eye(2)/d1 - ((X(1:2)-a1(1:2))*(X(1:2)-a1(1:2))')/(d1^3)) ...
+                                            -(eye(2)/d2 - ((X(1:2)-a2(1:2))*(X(1:2)-a2(1:2))')/(d2^3)) );
+                J(1:2,3) = gradS;
+                J(3,1:3) = [gradS; 0]';
+
+                % update for the candidate solution
+                delta = J\F;
+
+                % backtracking line search to ensure convergence if prior is far
+                % from root
+                i = 0;
+                while(true)
+                    X_new = X - (0.5^i) * delta;
+
+                    d1 = norm([X_new(1:2);forceZ]-a1);
+                    d2 = norm([X_new(1:2);forceZ]-a2);
+                    gradS = (X_new(1:2)-a1(1:2))/d1 - (X_new(1:2)-a2(1:2))/d2;
+                    F_new(1:2) = X_new(1:2) + X_new(3)*gradS - prior(1:2)';
+                    F_new(3) = d1 - d2 - uwb.distance;
+                    if (norm(F_new)<=norm(F))
+                        break;
+                    elseif (i>timeout_max_i)
+                        % timeout if no convergence for too long
+                        exit_timeout = 1;
+                        break;
+                    else
+                        i = i+1;
+                    end
+                end
+                X = X_new;
+                if norm(delta)<0.001 || norm(F)<0.001
+                    break;
+                elseif (exit_timeout)
+                    % forced exit by timeout
+                    break;
+                end
+            end
+            position = [X(1:2);forceZ]';
+        end
+    end
+end
+
