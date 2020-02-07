54b8fa5c820eb9f476134ce10ceb4c4340315329
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-02-07 15:26:57 +0100

    Included support for addition and subtraction in QuadrotorState class

--- Classes/QuadrotorState.m
+++ Classes/QuadrotorState.m
@@ -16,6 +16,24 @@ classdef QuadrotorState
             %QuadrotorState Construct an instance of this class
             
         end
+        function r = plus(obj1, obj2)
+            r = QuadrotorState;
+            r.p = obj1.p + obj2.p;
+            r.v = obj1.v + obj2.v;
+            r.roll = obj1.roll + obj2.roll;
+            r.pitch = obj1.pitch + obj2.pitch;
+            r.yaw = obj1.yaw + obj2.yaw;
+            r.timestamp = min([obj1.timestamp, obj2.timestamp]);
+        end
+        function r = minus(obj1, obj2)
+            r = QuadrotorState;
+            r.p = obj1.p - obj2.p;
+            r.v = obj1.v - obj2.v;
+            r.roll = obj1.roll - obj2.roll;
+            r.pitch = obj1.pitch - obj2.pitch;
+            r.yaw = obj1.yaw - obj2.yaw;
+            r.timestamp = min([obj1.timestamp, obj2.timestamp]);
+        end    
     end
 end
 
