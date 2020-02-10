25626a0efc6e4a22ea27cdd43231aa71fc224fce
SUPfeiffer <sven.pfeiffer@hotmail.com>
2020-02-10 17:12:33 +0100

    Moved moving horizon estimator to system for more modularity

--- Estimator_MovingHorizon.m
+++ Estimator_MovingHorizon.m
@@ -4,8 +4,9 @@ close all;
 %clear all;
 addpath('Functions');
 addpath('Classes');
+addpath('Systems');
 %load('Data/square.mat')
-load('simulationData_test.mat');
+%load('simulationData_test.mat');
 %% Parameters
 pos_update_dt = 0.01;
 mhe_timeHorizon = 0.5; % s
@@ -25,6 +26,8 @@ g = 9.81;
 state_prediction = QuadrotorState();
 state_corrector = QuadrotorState();
 state_estimate = QuadrotorState();
+measurement = QuadrotorState();
+estimator = MovingHorizonEstimator();
 
 % initial values
 pos0 = [data.x(1),data.y(1),data.z(1)];
@@ -52,20 +55,7 @@ mode = data.uwb(1).rangingMode;
 for i=10:N
     time_now = data.time(i);
     if(mod(time_now,pos_update_dt) == 0)
-        % Prediction 
-        pitch_global = data.pitch(i) * cos(data.yaw(i)) - data.roll(i)*sin(data.yaw(i));
-        roll_global = data.pitch(i) * sin(data.yaw(i)) + data.roll(i)*cos(data.yaw(i));
-        
-        state_prediction.p(1) = state_prediction.p(1) + state_prediction.v(1) * pos_update_dt;
-        state_prediction.p(2) = state_prediction.p(2) + state_prediction.v(2) * pos_update_dt;
-        
-        state_prediction.v(1) = state_prediction.v(1)...
-                                + (-g*tan(pitch_global) - k_aero*state_prediction.v(1)) * pos_update_dt;
-        state_prediction.v(2) = state_prediction.v(2)...
-                                + (g*tan(roll_global) - k_aero*state_prediction.v(2)) * pos_update_dt;
-        log.prediction(i,:) = state_prediction.p; 
-        
-        % Measurement
+        %% Measurement
         measurement_update = 0;
         %%%%%%%%%%%
         % Multilateration & Variations
@@ -94,7 +84,11 @@ for i=10:N
             uwb_mem = uwb_mat(uwb_logical);
         end
         
-        measurement = uwbMultilat(uwb_mem, 'prior', state_estimate.p,'nbest',10);%, 'forceZ', data.z(i));
+        measurement.p = uwbMultilat(uwb_mem, 'prior', state_estimate.p,'nbest',10);%, 'forceZ', data.z(i));
+        measurement.pitch = data.pitch(i);
+        measurement.roll = data.roll(i);
+        measurement.yaw = data.yaw(i);
+        measurement.timestamp = time_now;
         
         %%%%%%%%%%%
         % Single Measurement Projection & Variations
@@ -106,84 +100,12 @@ for i=10:N
 %         measurement = prior;
         %%%%%%%%%%%
         
-        log.measurement.x(i) = measurement(1);
-        log.measurement.y(i) = measurement(2);
-        log.measurement.z(i) = measurement(3);
+        log.measurement.x(i) = measurement.p(1);
+        log.measurement.y(i) = measurement.p(2);
+        log.measurement.z(i) = measurement.p(3);
         
-        if measurement_update
-            % Update moving windows
-            wPrediction = [state_prediction.p; wPrediction(1:end-1,:)];
-            wMeasurement = [measurement; wMeasurement(1:end-1,:)];
-            wTime = [time_now; wTime(1:end-1)];
-            
-            if (windowSize < MAX_WINDOW_SIZE)
-                windowSize = windowSize + 1;
-            end
-            while ((wTime(1)-wTime(windowSize)) > mhe_timeHorizon)
-                windowSize = windowSize -1;
-            end
-            
-            % calculate delta windows
-            wDt = wTime - wTime(windowSize);
-            wDp = wMeasurement - wPrediction;
+        state_estimate = estimator(measurement,time_now);
 
-            % RANSAC MHE
-            if(windowSize >= ransac_sampleSize && sum(wDt>0)>ransac_sampleSize-1)
-                best_error = windowSize * ransac_outlierThreshold + 1;
-
-                for j=1:ransac_iter
-                    idx = randsample(windowSize,ransac_sampleSize);
-
-                    if (ransac_sampleSize == 2)
-                        denom = (wDt(idx(2)) - wDt(idx(1)))^2 + 2*PV;
-
-                        s_dp = (( wDp(idx(1),:) * wDt(idx(2)) - wDp(idx(2),:) * wDt(idx(1)) ) ...
-                                * (wDt(idx(2)) - wDt(idx(1))) + (wDp(idx(1),:) + wDp(idx(2),:)) * PV)/denom;
-                        s_dv = ( wDp(idx(2),:) - wDp(idx(1),:) ) * (wDt(idx(2)) - wDt(idx(1))) / denom;
-                    else
-                        [s_dp(1),s_dv(1)] = linearLeastSquares(wDt(idx),wDp(idx,1),ransac_prior);
-                        [s_dp(2),s_dv(2)] = linearLeastSquares(wDt(idx),wDp(idx,2),ransac_prior);
-                        [s_dp(3),s_dv(3)] = linearLeastSquares(wDt(idx),wDp(idx,3),ransac_prior);
-                    end
-
-                    step_error = s_dv.*wDt(1:windowSize) + s_dp - wDp(1:windowSize,:);
-                    step_error = sqrt(sum(step_error.^2,2)); % mean squared error for each timestep
-                    isOutlier = (step_error>ransac_outlierThreshold); % identify outliers
-                    step_error(isOutlier) = ransac_outlierThreshold; % prune outlier error
-                    total_error = sum(step_error);
-
-                    % Update best model
-                    if (total_error < best_error)
-                       best_error = total_error;
-                       best_isOutlier = isOutlier;
-                       best_dp = s_dp;
-                       best_dv = s_dv;
-                    end
-                end
-
-                % Recalculate best model from inliers
-                if (sum(~best_isOutlier)>=2)
-                    dpos_in = wDp(~best_isOutlier,:);
-                    dt_in = wDt(~best_isOutlier);
-
-                    [state_corrector.p(1), state_corrector.v(1)] = linearLeastSquares(dt_in,dpos_in(:,1),ransac_prior);
-                    [state_corrector.p(2), state_corrector.v(2)] = linearLeastSquares(dt_in,dpos_in(:,2),ransac_prior);
-                    [state_corrector.p(3), state_corrector.v(3)] = linearLeastSquares(dt_in,dpos_in(:,3),ransac_prior);
-                else
-                    state_corrector.p = best_dp;
-                    state_corrector.v = best_dv;
-                end
-
-            end
-
-        end
-        if windowSize > 0
-            delta_t = (data.time(i) - wTime(windowSize))/1000;
-        else
-            delta_t = data.time(i);
-        end
-        state_estimate.p = state_prediction.p + state_corrector.p + state_corrector.v*delta_t;
-        state_estimate.v = state_prediction.v + state_corrector.v;
     end
     
     log.time(i) = data.time(i);
@@ -293,4 +215,4 @@ grid('on')
 grid minor
 plot3(data.x,data.y,data.z,'b')
 plot3(log.measurement.x, log.measurement.y, log.measurement.z ,'x')
-%plot3(log.x,log.y,log.z,'r')
+plot3(log.x,log.y,log.z,'r')
---
+++ Systems/MovingHorizonEstimator.m
@@ -0,0 +1,166 @@
+classdef MovingHorizonEstimator < matlab.System
+    % MovingHorizonEstimator
+    %
+    % FIR state estimator based on S.Li 2019
+    % (https://arxiv.org/abs/1905.10110)
+
+    % Public, tunable properties
+    properties
+
+    end
+
+    properties(DiscreteState)
+        x_pred
+        v_pred
+        t_pred
+        dx_est
+        dv_est
+        last_error_update
+        wt
+        wDp
+        windowSize
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+        initialState = QuadrotorState();
+        % Model
+        g = 9.81;   % gravitational constant
+        k = 0.35;   % drag coefficient
+        % MHE
+        maxWindowSize = 100;
+        timeHorizon = 0.15;
+        % RANSAC
+        ransacIterations = 10;
+        ransacSamples = 2;
+        ransacPrior = [0, 0; 0, 50];
+        outlierThreshold = 0.5;
+    end
+
+    methods
+        function obj = MovingHorizonEstimator(varargin)
+            % Support name-value pair arguments when constructing object
+            setProperties(obj,nargin,varargin{:})
+        end
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+        end
+
+        function est = stepImpl(obj, measurement, time)
+            % Attittude measurement is correct
+            attitude.pitch = measurement.pitch;
+            attitude.roll = measurement.roll;
+            attitude.yaw = measurement.yaw;
+            
+            % Prediction
+            obj.updatePrediction(attitude, time);
+            
+            if measurement.timestamp > obj.last_error_update
+                wDt = obj.updateWindows(measurement, time);
+                if(obj.windowSize >= obj.ransacSamples && sum(wDt>0)>obj.ransacSamples-1)
+                    best_error = obj.windowSize * obj.outlierThreshold + 1;
+
+                    for i=1:obj.ransacIterations
+                        idx = randsample(obj.windowSize,obj.ransacSamples);
+
+                        if (obj.ransacSamples == 2)
+                            PV = obj.ransacPrior(2,2);
+                            denom = (wDt(idx(2)) - wDt(idx(1)))^2 + 2*PV;
+
+                            s_dp = (( obj.wDp(idx(1),:) * wDt(idx(2)) - obj.wDp(idx(2),:) * wDt(idx(1)) ) ...
+                                    * (wDt(idx(2)) - wDt(idx(1))) + (obj.wDp(idx(1),:) + obj.wDp(idx(2),:)) * PV)/denom;
+                            s_dv = ( obj.wDp(idx(2),:) - obj.wDp(idx(1),:) ) * (wDt(idx(2)) - wDt(idx(1))) / denom;
+                        else
+                            [s_dp(1),s_dv(1)] = linearLeastSquares(wDt(idx),obj.wDp(idx,1),obj.ransacPrior);
+                            [s_dp(2),s_dv(2)] = linearLeastSquares(wDt(idx),obj.wDp(idx,2),obj.ransacPrior);
+                            [s_dp(3),s_dv(3)] = linearLeastSquares(wDt(idx),obj.wDp(idx,3),obj.ransacPrior);
+                        end
+
+                        step_error = s_dv.*wDt(1:obj.windowSize) + s_dp - obj.wDp(1:obj.windowSize,:);
+                        step_error = sqrt(sum(step_error.^2,2)); % mean squared error for each timestep
+                        isOutlier = (step_error>obj.outlierThreshold); % identify outliers
+                        step_error(isOutlier) = obj.outlierThreshold; % prune outlier error
+                        total_error = sum(step_error);
+
+                        % Update best model
+                        if (total_error < best_error)
+                           best_error = total_error;
+                           best_isOutlier = isOutlier;
+                           best_dp = s_dp;
+                           best_dv = s_dv;
+                        end
+                    end
+
+                    % Recalculate best model from inliers
+                    if (sum(~best_isOutlier)>=2)
+                        dpos_in = obj.wDp(~best_isOutlier,:);
+                        dt_in = wDt(~best_isOutlier);
+
+                        [obj.dx_est(1), obj.dv_est(1)] = linearLeastSquares(dt_in,dpos_in(:,1),obj.ransacPrior);
+                        [obj.dx_est(2), obj.dv_est(2)] = linearLeastSquares(dt_in,dpos_in(:,2),obj.ransacPrior);
+                        [obj.dx_est(3), obj.dv_est(3)] = linearLeastSquares(dt_in,dpos_in(:,3),obj.ransacPrior);
+                    else
+                        obj.dx_est = best_dp;
+                        obj.dv_est = best_dv;
+                    end
+                    obj.last_error_update = time;
+                end   
+            end
+            if obj.windowSize == 0 % first iteration
+                dt = 0;
+            else
+                dt = time - obj.wt(obj.windowSize);
+            end
+            
+            est = QuadrotorState();
+            est.p = obj.x_pred + obj.dx_est + dt*obj.dv_est;
+            est.v = obj.v_pred + obj.dv_est;
+            est.timestamp = time;
+        end
+
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+            obj.x_pred = obj.initialState.p;
+            obj.v_pred = obj.initialState.v;
+            obj.t_pred = 0;
+            obj.dx_est = [0,0,0];
+            obj.dv_est = [0,0,0];
+            obj.last_error_update = 0;
+            obj.wt = zeros(obj.maxWindowSize,1);
+            obj.wDp = zeros(obj.maxWindowSize,3);
+            obj.windowSize = 0;
+        end
+    end
+    
+    methods(Access = private)
+        function obj = updatePrediction(obj,attitude,time)
+            dt = time - obj.t_pred;
+            obj.t_pred = time;
+            
+            p_global = attitude.pitch * cos(attitude.yaw) - attitude.roll*sin(attitude.yaw);
+            r_global = attitude.pitch * sin(attitude.yaw) + attitude.roll*cos(attitude.yaw);
+        
+            obj.x_pred(1) = obj.x_pred(1) + obj.v_pred(1) * dt;
+            obj.x_pred(2) = obj.x_pred(2) + obj.v_pred(2) * dt;
+        
+            obj.v_pred(1) = obj.v_pred(1) + (-obj.g*tan(p_global) - obj.k*obj.v_pred(1)) * dt;
+            obj.v_pred(2) = obj.v_pred(2) + ( obj.g*tan(r_global) - obj.k*obj.v_pred(2)) * dt;
+        end
+        
+        function wDt = updateWindows(obj, measurement, time)
+            obj.wt = [time; obj.wt(1:end-1)];
+            obj.wDp = [measurement.p - obj.x_pred; obj.wDp(1:end-1,:)];
+            
+            if (obj.windowSize < obj.maxWindowSize)
+                obj.windowSize = obj.windowSize + 1;
+            end
+            while ((obj.wt(1)-obj.wt(obj.windowSize)) > obj.timeHorizon)
+                obj.windowSize = obj.windowSize -1;
+            end
+            wDt = obj.wt - obj.wt(obj.windowSize);           
+        end    
+    end
+end
